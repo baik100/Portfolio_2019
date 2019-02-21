124942a0e0843ffd0fb76a1385de78d676214f07
Baik <bcy0729@naver.com>
2019-02-21 16:42:41 +0900

    2차 수정

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,12 +1,4 @@
-
-
-
-
-
-
-
-
-$(document).ready(function() {
+$(document).ready(function () {
 
 
     /**
@@ -20,70 +12,76 @@ $(document).ready(function() {
      *v lib
      */
 
-    lib.init();
-
 });
 
 
-var portFolio ={
+var portFolio = {
     init: function () {
-        portFolio.isScroll(500, "html", "is-scroll");
-        portFolio.isScroll(1000, ".back-top", "is-active");
         portFolio.spyScroll();
         portFolio.sideMenu();
         portFolio.anim();
-        portFolio.backTop();
         portFolio.visualAnim();
         portFolio.preLoader();
+        portFolio.backTop();
         portFolio.responsiveReset();
+        portFolio.scrollToSection(".enabled-scroll", ".work");
+        portFolio.scrollToSection(".back-top", "body");
+        portFolio.handleMouseWheel('.visual', '.work');
     },
 
-    responsiveReset: function(){
+    responsiveReset: function () {
         $(window).resize(function () {
             $('html').removeClass('is-sideopen');
         })
     },
 
     preLoader: function () {
-        $(window).on('load',function () {
+        $(window).on('load', function () {
             $('.holder').delay(1500).fadeOut();
         })
     },
 
-
-    isScroll : function (point, el, className) {
-        $(window).scroll(function () {
-            var sct = $(window).scrollTop();
-            console.log(sct);
-
-            if(sct > point){
-                $(el).addClass(className);
-            }else{
-                $(el).removeClass(className);
-            }
-        })
-    },
-
     spyScroll: function () {
-        $('.nav-link').on("click",function () {
-            var self = $(this),
+        let currentSectionIndex = 0;
+
+        $('.nav-link').on("click", function () {
+            const self = $(this),
                 dataNav = self.data('nav');
 
             // 예외처리
-            if(dataNav === undefined || dataNav === null || dataNav.length <= 0){
+            if (dataNav === undefined || dataNav === null || dataNav.length <= 0) {
                 return;
             }
 
             console.log($(dataNav).offset());
-            var oft = $(dataNav).offset().top;
+            const oft = $(dataNav).offset().top;
 
-            $('html,body').animate({scrollTop: oft})
+            $('html,body').animate({scrollTop: oft});
         })
 
+        $(window).on('scroll', function () {
+            const sct = $(window).scrollTop();
+            const sections = $('section');
+
+            sections.each(function (index) {
+                const self = $(this);
+                const oft = self.offset().top;
+
+                if (sct > oft) {
+                    self.addClass('is-current-section').siblings().removeClass('is-current-section')
+                    currentSectionIndex = index;
+                }
+            })
+
+            $('.nav-link').eq(currentSectionIndex).addClass('on').siblings().removeClass('on');
+
+            console.log(currentSectionIndex)
+
+        })
     },
 
-    sideMenu : function () {
-        $('.btn-menu').click(function (){
+    sideMenu: function () {
+        $('.btn-menu').click(function () {
             $('html').addClass("is-sideopen");
         })
 
@@ -91,44 +89,74 @@ var portFolio ={
             $("html").removeClass("is-sideopen");
         })
 
+        $('.nav-link').on('click', function (e) {
+            e.preventDefault();
+            $("html").removeClass("is-sideopen");
+        })
+
     },
 
     anim: function () {
-        $(window).on('load',function () {
-            TweenMax.from('.visual h2', 1, {y: 50, opacity: 0, delay:2});
-            TweenMax.from('.visual p', 2, {y: 50, opacity: 0, delay:2.1});
+        $(window).on('load', function () {
+            TweenMax.from('.visual h2', 1, {y: 50, opacity: 0, delay: 2});
+            TweenMax.from('.visual p', 2, {y: 50, opacity: 0, delay: 2.1});
         })
     },
 
     backTop: function () {
-        $('.back-top').on('click',function () {
-            $('html,body').animate({ scrollTop: 0 });
-        })
+
+        const backTop = $('.back-top');
+
+        $(window).on('scroll', function () {
+            const sct = $(window).scrollTop();
+            if (sct > 300) {
+                backTop.addClass('active')
+            } else {
+                backTop.removeClass('active')
+            }
+        });
     },
 
     visualAnim: function () {
-        $(window).on('scroll',function () {
+        $(window).on('scroll', function () {
             const sct = $(window).scrollTop();
-            $('.visual').css({backgroundSize: 100 + sct / 100  +"%"})
-            $('.visual h3 , .visual p').css({opacity: 1 - sct /300})
-            $('.visual .text').css({transform: "translateY("
-                    + sct / 3 + "px)"})
-        })
-    },
-};
-
-const lib = {
-    init: function () {
-        $.fn.extend({
-            a: "sdfsdfsdfsdf",
-            aa:"sdfsdfsdfsdf,"
+            $('.visual').css({backgroundSize: 100 + sct / 100 + "%"})
+            $('.visual h3 , .visual p').css({opacity: 1 - sct / 300})
+            $('.visual .text').css({
+                transform: "translateY(" + sct / 3 + "px)"
+            })
         })
     },
 
+    scrollToSection: function (el, section) {
+        $(el).on('click', function () {
+            const $section = $(section);
+            if ($section.length > 0) {
+                const sectionOft = $section.offset().top;
+                $('html,body').animate({scrollTop: sectionOft});
+            }
+        })
+    },
+
+    handleMouseWheel: function (el, section) {
+        const $el = $(el);
+        mouseWheelTrigger();
+
+        $el.on('nextSection',function () {
+            const sectionOft = $(section).offset().top;
+            $el.off('mousewheel');
+
+            $('html, body').animate({scrollTop: sectionOft}, 600)
+            setTimeout(mouseWheelTrigger, 2000)
+        });
+
+        function mouseWheelTrigger() {
+            $el.on('mousewheel', function () {
+                $el.trigger('nextSection');
+            })
+        }
+    }
 };
-
-
-
 
 
 
